83ce154c71749b0d4565092b7e85f4a8b8a3b7d6
cormacio100 <cormac.music@gmail.com>
2018-02-14 04:43:57 +0000

    Updated static_root

--- manage.py
+++ manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "s3_upload_python.settings")
+    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "s3_upload_python.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")  # should change
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
