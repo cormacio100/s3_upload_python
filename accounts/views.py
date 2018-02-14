948632b3d4b5909cf91de70c6fd4c94339ff4bb2
cormacio100 <cormac.music@gmail.com>
2018-02-14 16:54:30 +0000

    updated object_name settings

--- accounts/views.py
+++ accounts/views.py
@@ -53,8 +53,8 @@ def sign_s3(request):
     AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
     S3_BUCKET = os.environ.get('S3_BUCKET')
 
-    #object_name = urllib.parse.quote_plus(request.GET['file_name'])
-    object_name = urllib.pathname2url(request.GET['file_name'])
+    object_name = urllib.parse.quote_plus(request.GET['file_name'])
+    #object_name = urllib.pathname2url(request.GET['file_name'])
     #object_name = urlparse.quote_plus(request.GET['file_name'])
     #object_name = urllib.quote_plus(request.GET['file_name'])from urlparse import urlparse
     mime_type = request.GET['file_type']
