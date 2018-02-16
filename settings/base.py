1c88d83d37bbd85191e18513c8e21cfd97d9123d
cormacio100 <cormac.music@gmail.com>
2018-02-16 02:28:31 +0000

    Added CORS setting

--- settings/base.py
+++ settings/base.py
@@ -115,3 +115,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 S3_BUCKET = os.environ.get("S3_BUCKET")
+
+CORS_ORIGIN_ALLOW_ALL = True
